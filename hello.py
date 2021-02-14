1639dcdee3a5057f3f512d90dcfa635b1b088388
shuya tobari <bali@hayashi.kenta>
2021-02-15 02:47:26 +0900

    file update

--- hello.py
+++ hello.py
@@ -271,4 +271,4 @@ def delete_all():
 #------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(debug=True, host='0.0.0.0', port=8000)
